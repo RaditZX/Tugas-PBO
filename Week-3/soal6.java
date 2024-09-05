a3962fb9f503927a02144f9bc2722cce9b9cef5e
RanditZX <daivaraditya36@gmail.com>
2024-09-05 21:30:06 +0700

    feat: update week-3

---
+++ Week-3/soal6.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class soal6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan 2 bilangan : ");
+        long bil1 = sc.nextLong();
+        long bil2 = sc.nextLong();
+
+        System.out.println((bil1 + bil2));
+        System.out.println((bil1 * bil2));
+
+
+    }
+}
